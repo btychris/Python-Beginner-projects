4263e443204b8e592f6ca506dcc56f5de4698d5f
DarknixDev <chrisbotty13@gmail.com>
2025-06-29 00:08:56 +0100

    Add files via upload

---
+++ countingin5.py
@@ -0,0 +1,10 @@
+# AI:Bot Training Challenges - www.101computing.net/for-loops-challenges
+
+# Counting in 2 from 0 to 100
+for i in range(0,505, 5):
+    print(i)
+
+
+
+
+
---
+++ writinglines.py
@@ -0,0 +1,5 @@
+# AI:Bot Training Challenges - www.101computing.net/for-loops-challenges
+
+# Writing Lines
+for i in range(0, 100):
+    print("I will listen to my teacher and complete my work on time.")
