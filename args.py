08f67eec0c9227f4392eaeef538c33a9a4a54bf1
Chris Botty <chrisbotty13@gmail.com>
2025-08-11 01:08:18 +0100

    Create args.py

---
+++ args.py
@@ -0,0 +1,5 @@
+def join_string(*args):
+    return " ".join(args)
+    
+print(join_string("Hello", "World", "!"))
+# *args is used to for when we dont know how many arguments are going to be inputted
